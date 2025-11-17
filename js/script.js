f9cb1353fd171b021ff6b9c84e20a9d8d9cc02bd
David Baquero <davidbaquerogonzalez@gmail.com>
2025-11-17 21:40:18 +0100

    Añadiendo código de la Live Review

--- js/script.js
+++ js/script.js
@@ -48,3 +48,57 @@ fetch('https://jsonplaceholder.typicode.com/users')
   .catch((error) => {
     usersList.innerHTML = `<li class="error">Hubo un error y no se pudo obtener los datos de los usuarios</li>`;
   });
+
+// 👇 CODIGO DE LA LIVE REVIEW EMPIEZA DESDE AQUÍ 👇
+
+// const listaUsuarios = document.getElementById("listaUsuarios")
+
+// function getUsers() {
+//   fetch("https://jsonplaceholder.typicode.com/users")
+//     .then(response => response.json())
+//     .then(data => {
+//       const users = data.map(user => {
+//         const { id, address } = user
+//         return {
+//           ...user,
+//           age: randomAge(18, 65),
+//           img: `../assets/img/${id}.jpeg`,
+//           address: `${address.street}, ${address.suite}, ${address.city}`,
+//         }
+//       }).map(user => {
+//         const { name, age, username, img, phone, email, company: { name: companyName }, address } = user
+//         const template = `
+//       <li>
+//         <div class="user-content">
+//           <div class="user-info">
+//             <h2><strong>Nombre:</strong> ${name}</h2>
+//             <p><strong>Edad:</strong> ${age}</p>
+//             <p><strong>Usuario:</strong> ${username}</p>
+//             <p><strong>Teléfono:</strong> ${phone}</p>
+//             <p><strong>Email:</strong> ${email}</p>
+//           </div>
+//           <div class="user-image">
+//             <img src="${img}" alt="${name}" /> 
+//           </div>
+//         </div>
+//         <div class="user-company">
+//           <p><strong>Compañía:</strong> ${companyName}</p>
+//           <p><strong>Dirección:</strong> ${address}</p>
+//         </div>
+//       </li>
+//       `
+//         return template
+//       }).join("")
+//       listaUsuarios.innerHTML = users
+
+//     })
+// }
+
+// function randomAge(min, max) {
+//   return Math.floor(Math.random() * (max - min + 1)) + min
+// }
+
+// getUsers()
+
+// console.log(randomAge(18, 65))
+// Math.floor(Math.random() * (65 - 18 + 1)) + 18 => El +1 es para que salga el número máximo, que en este ejemplo es 65.
